bb66ca699f1fac60f79e89f29a17d0d0404414bc
AH Mangorangca <lavahotsnacks@gmail.com>
2026-05-20 20:16:42 +0800

    wire guard into module and mark ping as public

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AttendanceController } from './attendance.controller';
+import { AuthModule } from './auth/auth.module';
+import { FirebaseAuthGuard } from './auth/firebase-auth.guard';
 import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [FirebaseModule, AuthModule],
   controllers: [AttendanceController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: FirebaseAuthGuard,
+    },
+  ],
 })
 export class AttendanceModule {}
